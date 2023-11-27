d2003122fcc36a5d71a3ee8849975089a741374b
Jaime Fuentes Mendieta <104949327+jaimemendieta@users.noreply.github.com>
2023-11-26 22:48:24 -0700

    Contact page update

--- bonanza/src/app/contact/page.tsx
+++ bonanza/src/app/contact/page.tsx
@@ -3,41 +3,61 @@ import React from 'react';
 const Page = () => {
     return (
         <main>
-            <section className="section section-other">
+            <section className="section section-other section-contact">
                 <div className="container__explained">
                     <div className="explanation__first">
                         <h1>I'm available! Let's talk</h1>
-                        <p>Reach out and I'll get back to you within 24 hours.</p>
+                        <h3>Reach out and I'll get back to you within 24 hours.</h3>
                     </div>
                 </div>
 
-                <form action="/submit-form" method="post">
-                    <label htmlFor="name">Name:</label>
-                    <input type="text" id="name" name="name" placeholder="Your name here" required>
-                    </input>
+                <div className="form-wrapper">
+                    <form action="/submit-form" method="post">
+                        <legend className="screen_reader_title">
+                            Contact Form
+                        </legend>
+                        <div className="field_container name-field-wrapper">
+                            <div className="t-container">
+                                <div className="t-cell">
+                                    <div className="el-group el-form-hide_label">
+                                        <div className="el-input--content">
+                                            <input className="el-form-control" type="text" id="name" name="name" placeholder="What's your name?" required>
+                                            </input>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
 
-                        <label htmlFor="email">Email:</label>
-                        <input type="email" id="email" name="email" placeholder="Your email here" required>
-                        </input>
+                        <div className="el-group el-form-hide_label">
+                            <div className="el-input--content">
+                                <input className="el-form-control" type="email" id="email" name="email" placeholder="Your email address?" required>
+                                </input>
+                            </div>
+                        </div>
 
+                        <div className="el-group el-form-hide_label">
+                            <div className="el-input--content">
+                                <select className="el-form-control" id="subject" name="subject" required defaultValue="">
+                                    <option value="" disabled>What is this about?</option>
+                                    <option value="option1">General Question</option>
+                                    <option value="option2">Insurance</option>
+                                    <option value="option3">Personalized Payments</option>
+                                    <option value="option4">Other</option>
+                                </select>
+                            </div>
+                        </div>
 
-                            <label htmlFor="subject">What is this about:</label>
-                            <select id="subject" name="subject" required>
-                                <option value="" disabled selected>Select one...</option>
-                                <option value="option1">Option 1</option>
-                                <option value="option2">Option 2</option>
-                                <option value="option3">Option 3</option>
-                            </select>
+                        <div className="el-group el-form-hide_label">
+                            <div className="el-input--content">
+                                <textarea className="el-form-control" id="message" name="message" placeholder="Feel free to message me any questions, or thoughts." required>
+                                </textarea>
+                            </div>
+                        </div>
 
-
-                            <label htmlFor="message">Tell me more:</label>
-                            <textarea id="message" name="message"
-                                      placeholder="Feel free to message me any questions, or thoughts."
-                                      ></textarea>
-
-
-                            <button type="submit">Submit</button>
-                </form>
+                        <button className=" el-group header-button" type="submit">Submit</button>
+                    </form>
+                </div>
 
             </section>
         </main>
