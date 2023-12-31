5c1f3fc2855eddce39eb6e24dad798349fe60051
Jaime Fuentes Mendieta <104949327+jaimemendieta@users.noreply.github.com>
2023-12-30 20:31:49 -0700

    Removed NextApiResponse and Request
    It caused build errors and removing it and using simple Web Request
    and Response solved the issue based on the documentation of Next

--- bonanza/src/app/api/route.ts
+++ bonanza/src/app/api/route.ts
@@ -1,69 +1,57 @@
-import type { NextApiRequest, NextApiResponse } from "next";
 import nodemailer from 'nodemailer';
 
-export async function POST(req: NextApiRequest, res: NextApiResponse) {
-    console.log('Received body:', req.body);
+export async function POST(req: Request, res: Response) {
 
-    if (req.method === 'POST') {
-        const transporter = nodemailer.createTransport({
-            host: 'mail.privateemail.com',
-            port: 465,
-            secure: true,
-            auth: {
-                user: process.env.EMAIL_USER,
-                pass: process.env.EMAIL_PASSWORD,
-            }
+    const transporter = nodemailer.createTransport({
+        host: 'mail.privateemail.com',
+        port: 465,
+        secure: true,
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASSWORD,
+        }
+    });
+
+    // Manually parse the JSON body from the request
+    const body = await req.body;
+    const data = await new Response(body).json();
+
+    const { name, email, subject, message } = data;
+
+    // Server validation
+    if (!name || !email || !subject || !message) {
+        return new Response(JSON.stringify({ error: 'All fields are required.' }), {
+            status: 400,
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+    }
+
+    const fullMessage = `Name: ${name}\nEmail: ${email}\nMessage:\n${message}`;
+
+    try {
+        const emailResponse = await transporter.sendMail({
+            from: 'giannina@bonanzacounseling.com',
+            replyTo: `"${name}" <${email}>`,
+            to: 'giannina@bonanzacounseling.com',
+            subject: subject,
+            text: fullMessage,
         });
 
-        // Manually parse the JSON body from the request
-        const body = await req.body;
-        const data = await new Response(body).json();
+        return new Response(JSON.stringify({ message: 'Email sent successfully' }), {
+            status: 200,
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
 
-        const { name, email, subject, message } = data;
-
-        // Server validation
-        if (!name || !email || !subject || !message) {
-            return new Response(JSON.stringify({ error: 'All fields are required.' }), {
-                status: 400,
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-            });
-        }
-
-        const fullMessage = `Name: ${name}\nEmail: ${email}\nMessage:\n${message}`;
-
-        try {
-            const emailResponse = await transporter.sendMail({
-                from: 'giannina@bonanzacounseling.com',
-                replyTo: `"${name}" <${email}>`,
-                to: 'giannina@bonanzacounseling.com',
-                subject: subject,
-                text: fullMessage,
-            });
-
-            return new Response(JSON.stringify({ message: 'Email sent successfully' }), {
-                status: 200,
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-            });
-
-        } catch (error) {
-            return new Response(JSON.stringify({ error: 'Error sending email', details: error }), {
-                status: 500,
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-            });
-        }
-    } else {
-        res.setHeader('Allow', ['POST']);
-        const headers = new Headers();
-        headers.set('Allow', 'POST');
-        return new Response(JSON.stringify(`Method ${req.method} Not Allowed`), {
-            status: 405,
-            headers: headers,
+    } catch (error) {
+        return new Response(JSON.stringify({ error: 'Error sending email', details: error }), {
+            status: 500,
+            headers: {
+                'Content-Type': 'application/json',
+            },
         });
     }
 }
